f5b42beedc8245e88c87e747487951a02f052e28
galstrom21 <jshepher@rackspace.com>
2012-07-14 23:21:35 -0500

    got screenshot support working

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,7 +2,7 @@ After do |scenario|
   if scenario.failed?
     Dir::mkdir('screenshots') if not File.directory?('screenshots')
     screenshot = "./screenshots/FAILED_#{scenario.name.gsub(' ','_').gsub(/[^0-9A-Za-z_]/, '')}.png"
-    Browser::BROWSER.driver.save_screenshot(screenshot)
+    @browser.driver.save_screenshot(screenshot)
     embed screenshot, 'image/png'
   end
 end
